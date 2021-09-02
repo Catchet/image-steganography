bf8b5a8e39eb44832f22ac14e93188dec4e6577c
Catchet <adam.agren@gmail.com>
2021-09-02 18:11:52 +0200

    Renamed some variables

--- steganography_create.py
+++ steganography_create.py
@@ -8,6 +8,7 @@ SecretImg = Image.open("secret_image.png")
 CombinedImg = Image.new("RGB", (PublicImg.size[0], PublicImg.size[1]), "black")  # Create an empty image with the same dimensions as PublicImg
 PublicPixels = PublicImg.load()
 SecretPixels = SecretImg.load()
+CombinedPixels = CombinedImg.load()
 
 PublicImg.show()     # Displays the public original image
 for i in range(PublicImg.size[0]):
@@ -15,14 +16,14 @@ for i in range(PublicImg.size[0]):
         rgb = [0, 0, 0]
         for k in range(3):
             rgb[k] = (PublicPixels[i, j][k] & ((1 << bits_total) - 2**bits_for_secret)) + (SecretPixels[i, j][k] >> (bits_total - bits_for_secret))
-        CombinedImg[i, j] = tuple(rgb)
+        CombinedPixels[i, j] = tuple(rgb)
 CombinedImg.show()   # Displays the combined image
 
 
 
 for i in range(CombinedImg.size[0]):
     for j in range(CombinedImg.size[1]):
-        SecretPixels[i, j] = tuple((x & (2**bits_for_secret - 1)) << (bits_total - bits_for_secret) for x in CombinedImg[i, j])
+        SecretPixels[i, j] = tuple((x & (2**bits_for_secret - 1)) << (bits_total - bits_for_secret) for x in CombinedPixels[i, j])
 SecretImg.show()     # Displays the extracted secret image
 
-CombinedImg.save("combined_image.png")  # Save the combined image
+CombinedImg.save("combined_img.png")  # Save the combined image
